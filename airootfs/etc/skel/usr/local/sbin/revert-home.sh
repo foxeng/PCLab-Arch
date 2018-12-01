39fd3789b35947e42830ebf46e3551fbef28a3a1
Fotis Xenakis <44291507+foxeng@users.noreply.github.com>
2018-12-01 16:24:09 +0200

    Do a recursive unmount of /home

--- airootfs/etc/skel/usr/local/sbin/revert-home.sh
+++ airootfs/etc/skel/usr/local/sbin/revert-home.sh
@@ -3,7 +3,7 @@
 # Revert /home before login (assuming we are successfully booted already, /home
 # should be mounted).
 
-umount /home
+umount -R /home
 /usr/local/sbin/rec-sub-del.sh home /root/btrfs-root
 btrfs subvolume snapshot /root/btrfs-root/home-snap /root/btrfs-root/home
 mount /home
