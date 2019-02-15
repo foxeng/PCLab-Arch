680c631c0819040a5bd4196b5317c055994a0a4c
Fotis Xenakis <foxen@windowslive.com>
2019-02-15 03:12:43 +0200

    Revert home: fall back to copying if unmounting fails

--- airootfs/etc/skel/usr/local/sbin/revert-home.sh
+++ airootfs/etc/skel/usr/local/sbin/revert-home.sh
@@ -3,7 +3,13 @@
 # Revert /home before login (assuming we are successfully booted already, /home
 # should be mounted).
 
-umount -R /home
-/usr/local/sbin/rec-sub-del.sh home /root/btrfs-root
-btrfs subvolume snapshot /root/btrfs-root/home-snap /root/btrfs-root/home
-mount /home
+if umount -R /home
+then
+	/usr/local/sbin/rec-sub-del.sh home /root/btrfs-root
+	btrfs subvolume snapshot /root/btrfs-root/home-snap /root/btrfs-root/home
+	mount /home
+else
+	rm -rf /home/labuser/
+	cp -r /root/btrfs-root/home-snap/labuser/ /home/	# could use --reflink
+	chown -R labuser:labuser /home/labuser/
+fi
