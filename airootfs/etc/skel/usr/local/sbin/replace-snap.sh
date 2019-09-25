5605c8c18b8806841c8d3738f22b8fff495a7055
Fotis Xenakis <foxen@windowslive.com>
2019-09-26 00:36:48 +0300

    Fix next boot after kernel upgrade

--- airootfs/etc/skel/usr/local/sbin/replace-snap.sh
+++ airootfs/etc/skel/usr/local/sbin/replace-snap.sh
@@ -7,3 +7,12 @@
 mv /root/btrfs-root/rootfs /root/btrfs-root/rootfs-tmp
 mv /root/btrfs-root/rootfs-snap /root/btrfs-root/rootfs
 mv /root/btrfs-root/rootfs-tmp /root/btrfs-root/rootfs-snap
+
+# Swapping the subvolumes above means that on the next boot, the bootloader will
+# load the kernel image in /root/btrfs-root/rootfs/boot, which is the OLD one
+# (the one from the previous snapshot). To correct that, we copy /boot from the
+# current system there.
+rm -rf /root/btrfs-root/rootfs/boot
+cp -r /boot /root/btrfs-root/rootfs/
+
+# TODO OPT: Keep a temporary backup of /boot on the snapshot?
