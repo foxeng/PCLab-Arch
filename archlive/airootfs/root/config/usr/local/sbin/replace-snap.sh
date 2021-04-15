98b73976db6add07ef4ddd12384f157fba898780
Fotis Xenakis <foxen@windowslive.com>
2021-04-15 22:12:20 +0300

    Revise installer

---
+++ archlive/airootfs/root/config/usr/local/sbin/replace-snap.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Replace root filesystem snapshot with one taken of the current filesystem
+# (when it powers off). In essence, make the system state when it is next
+# powered off permanent.
+
+mv /root/btrfs-root/rootfs /root/btrfs-root/rootfs-tmp
+mv /root/btrfs-root/rootfs-snap /root/btrfs-root/rootfs
+mv /root/btrfs-root/rootfs-tmp /root/btrfs-root/rootfs-snap
+
+# Swapping the subvolumes above means that on the next boot, the bootloader will
+# load the kernel image in /root/btrfs-root/rootfs/boot, which is the OLD one
+# (the one from the previous snapshot). To correct that, we copy /boot from the
+# current system there.
+rm -rf /root/btrfs-root/rootfs/boot
+cp -r /boot /root/btrfs-root/rootfs/
+
+# TODO OPT: Keep a temporary backup of /boot on the snapshot?
---
+++ archlive/airootfs/root/install-chroot.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+# Script to perform initial system configuration inside chroot
+
+set -v
+set -o pipefail
+
+# Figure out which device we are on
+# TODO OPT: Receive as argument
+disk=$(lsblk --nodeps --noheadings --list --output PKNAME \
+	$(findmnt --noheadings --list --nofsroot --output SOURCE / \
+		| head --lines 1)) || exit 1
+echo "chroot disk=${disk}"
+# Enable TRIM, if supported
+trim=$(lsblk --nodeps --noheadings --list --output DISC-GRAN "/dev/${disk}" \
+	| head --lines 1 | xargs) || exit 1   # xargs to remove whitespace
+[[ "${trim}" != "0B" ]] && systemctl enable fstrim.timer
+
+# Timezone
+# We can't use timedatectl under a chroot because it requires an active dbus
+# (see note in https://wiki.archlinux.org/index.php/Time#Time_standard)
+ln -sf /usr/share/zoneinfo/Europe/Athens /etc/localtime &&
+	hwclock --systohc --localtime || exit 1 # necessary to create /etc/adjtime
+
+# Localization
+# Uncomment en_US.UTF-8 UTF-8 and el_GR.UTF-8 UTF-8 in /etc/locale.gen
+sed -i -e 's/#en_US\.UTF-8 UTF-8/en_US\.UTF-8 UTF-8/g
+	   s/#el_GR\.UTF-8 UTF-8/el_GR\.UTF-8 UTF-8/g' /etc/locale.gen &&
+	locale-gen &&
+	cat > /etc/locale.conf <<-'EOF' || exit 1
+	LANG=en_US.UTF-8
+	LC_NUMERIC=el_GR.UTF-8
+	LC_TIME=el_GR.UTF-8
+	LC_MONETARY=el_GR.UTF-8
+	LC_PAPER=el_GR.UTF-8
+	LC_MEASUREMENT=el_GR.UTF-8
+	EOF
+
+# Network configuration
+hostname="${1}"
+echo ${hostname} > /etc/hostname &&
+	cat >> /etc/hosts <<-EOF &&
+	127.0.0.1	localhost
+	::1		localhost
+	127.0.1.1	${hostname}.localdomain	${hostname}
+	EOF
+	cat > /etc/systemd/network/20-wired.network <<-EOF &&
+	[Match]
+	Type=ether
+
+	[Network]
+	DHCP=yes
+	EOF
+	systemctl enable systemd-networkd.service systemd-resolved.service || exit 1
+
+# Initramfs
+# Add 'revert' as the last hook in mkinitcpio.conf
+gawk -i inplace -f - /etc/mkinitcpio.conf <<-'EOF' &&
+	BEGIN		{ FS = ")" }
+
+	/^HOOKS=/	{
+		print $1 " revert)"
+		next
+	}
+
+	{ print }	# Default action
+	EOF
+	mkinitcpio -P || exit 1
+
+# Root password
+root_password="${2}"
+echo "root:${root_password}" | chpasswd || exit 1
+
+# Configure SSH (root access, only with a key)
+ssh_key="${3}"
+install --directory --mode 700 /root/.ssh &&
+	echo "${ssh_key}" > /root/.ssh/authorized_keys &&
+	chmod 600 /root/.ssh/authorized_keys &&
+	cat >> /etc/ssh/sshd_config <<-'EOF' &&
+	# Disable password logins (only use SSH keys)
+	PasswordAuthentication no
+	EOF
+	systemctl enable sshd.service || exit 1
+
+# Boot loader
+# Check if / and /boot are on different devices to see if on UEFI or not
+if [[ $(stat -c '%d' /) = $(stat -c '%d' /boot) ]]; then
+	grub-install --target=i386-pc "/dev/${disk}" || exit 1
+else
+	grub-install \
+		--target=x86_64-efi \
+		--efi-directory=/boot \
+		--bootloader-id=GRUB || exit 1
+fi
+grub-mkconfig -o /boot/grub/grub.cfg || exit 1
